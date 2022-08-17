c8b82e11f72a882f97ec7ee5fc0d8d93cc6061bf
Jacob Goldfarb <jacobgoldfarb1997@gmail.com>
2022-08-17 13:03:29 -0400

    Interpreter working

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -2,6 +2,15 @@ package com.craftinginterpreters.lox;
 
 class Interpreter implements Expr.Visitor<Object> {
 
+    void interpret(Expr expression) {
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
+    }
+
     @Override
     public Object visitBinaryExpr(Expr.Binary expr) {
         Object left = evaluate(expr.left);
@@ -13,22 +22,32 @@ class Interpreter implements Expr.Visitor<Object> {
             case EQUAL_EQUAL: 
                 return isEqual(left, right);
             case GREATER:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left > (double)right;
             case GREATER_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left >= (double)right;
             case LESS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left < (double)right;
             case LESS_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left <= (double)right;
             case MINUS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left - (double)right;
             case PLUS:
                 if (left instanceof Double && right instanceof Double)
                     return (double)left + (double)right;
                 else if (left instanceof String && right instanceof String)
                     return (String)left + (String)right;
-                break;
+                else
+                    throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
             case SLASH:
+                checkNumberOperands(expr.operator, left, right);
+                return (double)left / (double)right;
+            case STAR:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left * (double)right;
         }
         return null;
@@ -52,10 +71,21 @@ class Interpreter implements Expr.Visitor<Object> {
             case BANG:
                 return !isTruthy(right);
             case MINUS:
+                checkNumberOperand(expr.operator, right);
                 return -(double)right;
         }
 
         return null;
+    }
+
+    private void checkNumberOperand(Token operator, Object operand) {
+        if (operand instanceof Double) return;
+        throw new RuntimeError(operator, "Operand must be a number.");
+    }
+
+    private void checkNumberOperands(Token operator, Object left, Object right) {
+        if (left instanceof Double && right instanceof Double) return;
+        throw new RuntimeError(operator, "Operands must be a numbers.");
     }
 
     private Object evaluate(Expr expr) {
@@ -74,6 +104,19 @@ class Interpreter implements Expr.Visitor<Object> {
         if (left == null & right == null) return  true;
         if (left == null) return false;
         return left.equals(right);
+    }
+
+    private String stringify(Object object) {
+        if (object == null) return "nil";
+        if (object instanceof  Double) {
+            String text = object.toString();
+            if (text.endsWith(".0")) {
+                text = text.substring(0, text.length() - 2);
+            }
+            return text;
+        }
+
+        return object.toString();
     }
     
 }
--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -9,7 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -28,6 +30,7 @@ public class Lox {
 
         // Indicate an error in the exite code.
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -43,8 +46,6 @@ public class Lox {
     }
 
     private static void run(String source) {
-        if (source == "") 
-            System.out.println("No source");
         Scanner scanner = new Scanner(source);
         // "50 + -3 * ( 2 - 1 )" -> [50, "+", "-", 3, "*", "(", 2, "-", 1, ")"]
         List<Token> tokens = scanner.scanTokens();
@@ -52,11 +53,10 @@ public class Lox {
         // [50, "+", "-", 3, "*", "(", 2, "-", 1, ")"] -> (/ (group (+ 1.0 2.0)) true)
         Parser parser = new Parser(tokens);
         Expr expression = parser.parse();
-        new AstPrinter().print(expression);
+        // Stop if syntax error.
+        if (hadError) return;
 
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
@@ -76,5 +76,11 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + 
+        "\n[line: " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 }
